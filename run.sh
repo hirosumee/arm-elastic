1900ae2c32165c1b0796220bc98f9c3ebde37fb7
Lars Larsson <lars.la@gmail.com>
2015-11-22 15:01:38 +0100

    make sure dirs exists

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ LOG_DIR=${LOG_DIR:-"$DATA_DIR/logs"}
 CLUSTER_NAME=${CLUSTER_NAME:-elasticsearch}
 LISTEN=${LISTEN:-127.0.0.1}
 
+mkdir -p $DATA_DIR
+mkdir -p $LOG_DIR
 chown -R elasticsearch $DATA_DIR
 chown -R elasticsearch $LOG_DIR
 
